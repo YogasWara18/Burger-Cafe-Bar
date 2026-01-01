522d60374e6f551729ce11fd13093b39b18df25f
YogasWara18 <teukukurniayogaswara@gmail.com>
2026-01-02 04:20:56 +0700

    update page

--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -1,47 +1,48 @@
-'use client';
+"use client";
 
-import React, {useEffect} from 'react';
-import Glightbox from 'glightbox';
-import './hero.css';
-import HeroBtn from '../components/HeroBtn';
+import React, { useEffect } from "react";
+import Glightbox from "glightbox";
+import "./hero.css";
+import HeroBtn from "../components/HeroBtn";
 
 export default function Hero() {
-    useEffect(() => {
-        new Glightbox({
-            selector: '.glightbox',
-        });
-    }, []);
+  useEffect(() => {
+    Glightbox({
+      selector: ".glightbox",
+    });
+  }, []);
 
   return (
-    <section id="hero" className='d-flex align-items-center'>
-        <div 
+    <section id="hero" className="d-flex align-items-center">
+      <div
         className="container position-relative text-center text-lg-start"
         data-aos="zoom-in"
         data-aos-delay="100"
-        >
-            <div className="row">
-                <div className="col-lg-8">
-                    <h1>Burger Cafe <span> & Bar</span></h1>
-                    <h2>Crafting burgers & bar moments since 2007</h2>
+      >
+        <div className="row">
+          <div className="col-lg-8">
+            <h1>
+              Burger Cafe <span> & Bar</span>
+            </h1>
+            <h2>Crafting burgers & bar moments since 2007</h2>
 
-                    <div className="btns">
-                        <HeroBtn name="Our menu" target="menu"/>
-                        <HeroBtn name="book a table" target="book-a-table"/>
-                    </div>
-                </div>
-                <div 
-                className="col-lg-4 d-flex align-items-center justify-content-center position-relative"
-                data-aos="zoom-in"
-                data-aos-delay="200"
-                >
-                    <a 
-                    href="https://www.youtube.com/watch?v=V5w1OGknhlc"
-                    className="glightbox play-btn"
-                    ></a>
-                </div>
+            <div className="btns">
+              <HeroBtn name="Our menu" target="menu" />
+              <HeroBtn name="book a table" target="book-a-table" />
             </div>
-
+          </div>
+          <div
+            className="col-lg-4 d-flex align-items-center justify-content-center position-relative"
+            data-aos="zoom-in"
+            data-aos-delay="200"
+          >
+            <a
+              href="https://www.youtube.com/watch?v=V5w1OGknhlc"
+              className="glightbox play-btn"
+            ></a>
+          </div>
         </div>
+      </div>
     </section>
-  )
+  );
 }
